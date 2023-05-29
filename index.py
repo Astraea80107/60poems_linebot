aca1ef42a176270107e27c47662d85baee79304a
Astraea80107 <134909509+Astraea80107@users.noreply.github.com>
2023-05-29 15:32:10 +0800

    Update index.py

--- index.py
+++ index.py
@@ -30,7 +30,7 @@ def checkword(w):
         msg += '\n'
     return msg
 
-w = input("請輸入要查詢的國字：")
+app = Flask(__name__)
 
 line_bot_api = LineBotApi('30fHYzc70eBZEscXrgWyuW0QQMPVGPd4R+CLHhdJAeokJrgn5OlH+TxOcfAzdvxErPxRtvmc6kDr5gvrrm31urWPPayGhThQvwbZ0E79cWH+8M2pjXbtiAgzvwoHX+BcHRnozscUh8i6LIZaUU1zZAdB04t89/1O/w1cDnyilFU=')
 handler1 = WebhookHandler('b2a58c0974beadd965204bda652ced11')
@@ -54,12 +54,11 @@ def callback():
 
     return 'OK'
 
-
 @handler1.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(checkword(event.message.text))
+        TextSendMessage(text=checkword(event.message.text)))
 
 if __name__ == "__main__":
     app.run()
