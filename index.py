491783ecb28b28e96ead9deca5c534bb58c97ac4
Astraea80107 <134909509+Astraea80107@users.noreply.github.com>
2023-06-01 11:41:06 +0800

    Update index.py

--- index.py
+++ index.py
@@ -10,25 +10,41 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
-import requests
+import twstock
 
-def checkword(w):
-    url = 'https://www.moedict.tw/uni/' + w
-    r = requests.get(url)
-    datas = r.json()
-    msg = '國字：' + datas['title'] + '\n'
-    msg += '部首：' + datas['radical'] + '\n'
-    msg += '筆劃：' + str(datas['stroke_count']) + '\n\n'
-    for i in range(len(datas['heteronyms'])):
-        msg += '注音：' + datas['heteronyms'][i]['bopomofo'] + '\n'
-        msg += '拼音：' + datas['heteronyms'][i]['pinyin'] + '\n'    
-        for j in range(len(datas['heteronyms'][i]['definitions'])):
-            if 'type' in datas['heteronyms'][i]['definitions'][j]:
-                msg += '[{}] {}\n'.format(
-                    datas['heteronyms'][i]['definitions'][j]['type'],
-                    datas['heteronyms'][i]['definitions'][j]['def'])
-        msg += '\n'
-    return msg
+twstock.realtime.mock = False
+
+def realtime(stock_id):    
+    stock_info = twstock.realtime.get(stock_id)
+
+    if stock_info['success'] == True:
+        best_bid_price = stock_info['realtime']['best_bid_price']
+        best_bid_volume = stock_info['realtime']['best_bid_volume']
+        best_ask_price = stock_info['realtime']['best_ask_price']
+        best_ask_volume = stock_info['realtime']['best_ask_volume']
+
+        msg = (f"""{stock_id}即時股價資訊
+
+股票名稱: {stock_info['info']['name']}
+股票代碼: {stock_info['info']['code']}
+資料時間: {stock_info['info']['time']}
+當前股價: {stock_info['realtime']['latest_trade_price']}
+開盤價: {float(stock_info['realtime']['open']):.2f}
+最高價: {float(stock_info['realtime']['high']):.2f}
+最低價: {float(stock_info['realtime']['low']):.2f}
+當前交易量: {stock_info['realtime']['trade_volume']}
+累積交易量: {stock_info['realtime']['accumulate_trade_volume']}
+當前5筆成交價: {float(best_bid_price[0]):.2f}, {float(best_bid_price[1]):.2f}, {float(best_bid_price[2]):.2f}, {float(best_bid_price[3]):.2f}, {float(best_bid_price[4]):.2f}
+當前5筆成交量: {int(best_bid_volume[0])}, {int(best_bid_volume[1])}, {int(best_bid_volume[2])}, {int(best_bid_volume[3])}, {int(best_bid_volume[4])}
+最佳5筆成交價: {float(best_ask_price[0]):.2f}, {float(best_ask_price[1]):.2f}, {float(best_ask_price[2]):.2f}, {float(best_ask_price[3]):.2f}, {float(best_ask_price[4]):.2f}
+最佳5筆成交量: {int(best_ask_volume[0])}, {int(best_ask_volume[1])}, {int(best_ask_volume[2])}, {int(best_ask_volume[3])}, {int(best_ask_volume[4])}
+""")
+
+        return msg
+    else:
+        error = "Data not found"
+
+        return error
 
 app = Flask(__name__)
 
@@ -59,7 +75,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=checkword(event.message.text)))
+        TextSendMessage(text=realtime(event.message.text)))
 
 
 if __name__ == "__main__":
